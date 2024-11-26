b0126ba4e3dc70d7f85e98aacae8d4255842ae27
uNik020 <shekharnikhil020@gmail.com>
2024-11-26 11:42:06 +0530

    fourth commit

--- rbac-dashboard/src/pages/UsersPage.js
+++ rbac-dashboard/src/pages/UsersPage.js
@@ -180,7 +180,7 @@ const UsersPage = () => {
                   <td className="px-4 py-2 border border-gray-300 dark:border-gray-700">{user.status}</td>
                   <td className="px-4 py-2 border border-gray-300 dark:border-gray-700">
                     <button
-                      onClick={() => handleEditClick(user)}
+                      onClick={() => setEditingUser(user)}
                       className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition"
                     >
                       Edit
